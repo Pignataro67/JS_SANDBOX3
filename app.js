c8d964db943fb66a64d07dc2e401e9ab29dffd64
Pignataro67 <Pignataro67@gmail.com>
2021-09-04 05:08:44 -0700

    fac pattern prac.

---
+++ app.js
@@ -0,0 +1,17 @@
+function memberFactory() {
+  this.createMember = function(name, type) {
+    let member;
+
+    if(type === 'simple') {
+      member = new SimpleMembership(name);
+    } else if (type === 'standard') {
+      member = new StandardMembership(name);
+    } else if (type === 'super') {
+      member = new SuperMembership(name);
+    }
+
+    member.type = type;
+
+
+  }
+}
