9df93fd117f6b1c67d715242bf36a1e8cc8b017b
Pignataro67 <Pignataro67@gmail.com>
2021-09-07 07:45:57 -0700

    more pattern practice, debug

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+function EventObserver() {
+  this.observers = [];
+}
+
+EventObserver.prototype = {
+  subscribe: function(fn) {
+    this.observers.push(fn)
+    console.log(`You are now subscribed to ${fn}.name`);
+  },
+  unsubscribe: function(fn) {
+    this.observers = this.observers.filter(function(item) {
+      if(item !== fn) {
+        return item;
+      }
+    });
+    console.log(`ou are now unsubscribed from ${fn.name`);
+  },
+  fire: function() {
+
+  }
+}
+
 
 
 // function memberFactory() {
