11b3df4af39d5a8714edbf4f3c366afb40d2d65d
Pignataro67 <Pignataro67@gmail.com>
2021-09-08 07:42:07 -0700

    some function improvement and html

--- app.js
+++ app.js
@@ -4,23 +4,44 @@ function EventObserver() {
 
 EventObserver.prototype = {
   subscribe: function(fn) {
-    this.observers.push(fn)
-    console.log(`You are now subscribed to ${fn}.name`);
+    this.observers.push(fn);
+    console.log(`You are now subscribed to ${fn.name}`);
   },
   unsubscribe: function(fn) {
+    // Fliter out from the list whatever matched the callback function. If there is no match, the callback gets to stay on the list. The filter returns a new list and reassigns the list of observers.
     this.observers = this.observers.filter(function(item) {
       if(item !== fn) {
         return item;
       }
     });
-    console.log(`ou are now unsubscribed from ${fn.name`);
+    console.log(`You are now unsubscribed from ${fn.name}`);
   },
   fire: function() {
-
+    this.observers.forEach(function(item) {
+      item.call();
+    });
   }
 }
 
+const click = new EventObserver();
 
+// Event Listeners
+document.querySelector('.sub-ms').addEventListener('click', function() {
+  click.subscribe(getCurMilliseconds);
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function() {
+  click.unsubscribe(getCurMilliseconds);
+});
+
+document.querySelector('.fire').addEventListener('click', function() {
+  click.fire();
+});
+
+// Click Handler
+const getCurMilliseconds = function() {
+  console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+}
 
 // function memberFactory() {
 //   this.createMember = function(name, type) {
