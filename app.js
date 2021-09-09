e872df1bc74b6971fe51aa3dd37d97f233a2c8d6
Pignataro67 <Pignataro67@gmail.com>
2021-09-09 05:08:03 -0700

    script change

--- app.js
+++ app.js
@@ -1,59 +1,61 @@
-function EventObserver() {
-  this.observers = [];
-}
 
-EventObserver.prototype = {
-  subscribe: function(fn) {
-    this.observers.push(fn);
-    console.log(`You are now subscribed to ${fn.name}`);
-  },
-  unsubscribe: function(fn) {
-    // Fliter out from the list whatever matched the callback function. If there is no match, the callback gets to stay on the list. The filter returns a new list and reassigns the list of observers.
-    this.observers = this.observers.filter(function(item) {
-      if(item !== fn) {
-        return item;
-      }
-    });
-    console.log(`You are now unsubscribed from ${fn.name}`);
-  },
-  fire: function() {
-    this.observers.forEach(function(item) {
-      item.call();
-    });
-  }
-}
 
-const click = new EventObserver();
+// function EventObserver() {
+//   this.observers = [];
+// }
 
-// Event Listeners
-document.querySelector('.sub-ms').addEventListener('click', function() {
-  click.subscribe(getCurMilliseconds);
-});
+// EventObserver.prototype = {
+//   subscribe: function(fn) {
+//     this.observers.push(fn);
+//     console.log(`You are now subscribed to ${fn.name}`);
+//   },
+//   unsubscribe: function(fn) {
+//     // Fliter out from the list whatever matched the callback function. If there is no match, the callback gets to stay on the list. The filter returns a new list and reassigns the list of observers.
+//     this.observers = this.observers.filter(function(item) {
+//       if(item !== fn) {
+//         return item;
+//       }
+//     });
+//     console.log(`You are now unsubscribed from ${fn.name}`);
+//   },
+//   fire: function() {
+//     this.observers.forEach(function(item) {
+//       item.call();
+//     });
+//   }
+// }
 
-document.querySelector('.unsub-ms').addEventListener('click', function() {
-  click.unsubscribe(getCurMilliseconds);
-});
+// const click = new EventObserver();
 
-document.querySelector('.sub-s').addEventListener('click', function() {
-  click.subscribe(getCurSeconds);
-});
+// // Event Listeners
+// document.querySelector('.sub-ms').addEventListener('click', function() {
+//   click.subscribe(getCurMilliseconds);
+// });
 
-document.querySelector('.unsub-ms').addEventListener('click', function() {
-  click.unsubscribe(getCurSeconds);
-});
+// document.querySelector('.unsub-ms').addEventListener('click', function() {
+//   click.unsubscribe(getCurMilliseconds);
+// });
 
-document.querySelector('.fire').addEventListener('click', function() {
-  click.fire();
-});
+// document.querySelector('.sub-s').addEventListener('click', function() {
+//   click.subscribe(getCurSeconds);
+// });
 
-// Click Handler
-const getCurMilliseconds = function() {
-  console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
-}
+// document.querySelector('.unsub-ms').addEventListener('click', function() {
+//   click.unsubscribe(getCurSeconds);
+// });
 
-const getCurSeconds = function() {
-  console.log(`Current Seconds: ${new Date().getSeconds()}`);
-}
+// document.querySelector('.fire').addEventListener('click', function() {
+//   click.fire();
+// });
+
+// // Click Handler
+// const getCurMilliseconds = function() {
+//   console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+// }
+
+// const getCurSeconds = function() {
+//   console.log(`Current Seconds: ${new Date().getSeconds()}`);
+// }
 
 // function memberFactory() {
 //   this.createMember = function(name, type) {
